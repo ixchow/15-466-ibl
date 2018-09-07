c23d5b17953236c27d54748915e178a75e654ced
Jim McCann (retchow) <ix@tchow.com>
2018-09-07 14:06:42 -0400

    Some dummy code for walkmesh walking

---
+++ WalkMesh.cpp
@@ -0,0 +1,35 @@
+#include "WalkMesh.hpp"
+
+WalkMesh(std::vector< glm::vec3 > const &vertices_, std::vector< glm::uvec3 > const &triangles_)
+	: vertices(vertices_), triangles(triangles_) {
+	//TODO: construct next_vertex map
+}
+
+WalkPoint WalkMesh::closest_point(glm::vec3 const &world_point) const {
+	WalkPoint closest;
+	//TODO: iterate through triangles
+	//TODO: for each triangle, find closest point on triangle to world_point
+	//TODO: if point is closest, closest.triangle gets the current triangle, closest.weights gets the barycentric coordinates
+	return closest;
+}
+
+void WalkMesh::walk(WalkPoint &wp, glm::vec3 const &step) const {
+	//TODO: project step to barycentric coordinates to get weights_step
+	glm::vec3 weights_step;
+
+	//TODO: when does wp.weights + t * weights_step cross a triangle edge?
+	float t = 1.0f;
+
+	if (t >= 1.0f) { //if a triangle edge is not crossed
+		//TODO: wp.weights gets moved by weights_step, nothing else needs to be done.
+
+	} else { //if a triangle edge is crossed
+		//TODO: wp.weights gets moved to triangle edge, and step gets reduced
+		//if there is another triangle over the edge:
+			//TODO: wp.triangle gets updated to adjacent triangle
+			//TODO: step gets rotated over the edge
+		//else if there is no other triangle over the edge:
+			//TODO: wp.triangle stays the same.
+			//TODO: step gets updated to slide along the edge
+	}
+}
---
+++ WalkMesh.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <vector>
+#include <unordered_map>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp> //allows the use of 'uvec2' as an unordered_map key
+
+struct WalkMesh {
+	//Walk mesh will keep track of triangles, vertices:
+	std::vector< glm::vec3 > vertices;
+	std::vector< glm::uvec3 > triangles; //CCW-oriented
+
+	//TODO: consider also loading vertex normals for interpolated "up" direction:
+	//std::vector< glm::vec3 > vertex_normals;
+
+	//This "next vertex" map includes [a,b]->c, [b,c]->a, and [c,a]->b for each triangle, and is useful for checking what's over an edge from a given point:
+	std::unordered_map< glm::uvec2, uint32_t > next_vertex;
+
+
+	//Construct new WalkMesh and build next_vertex structure:
+	WalkMesh(std::vector< glm::vec3 > const &vertices_, std::vector< glm::uvec3 > const &triangles_);
+
+	struct WalkPoint {
+		glm::uvec3 triangle = glm::uvec3(-1U); //indices of current triangle
+		glm::vec3 weights = glm::vec3(std::numeric_limits< float >::quiet_NaN()); //barycentric coordinates for current point
+	};
+
+	//used to initialize walking -- finds the closest point on the walk mesh:
+	// (should only need to call this at the start of a level)
+	WalkPoint closest_point(glm::vec3 const &world_point) const;
+
+	//used to update walk point:
+	void walk(WalkPoint &wp, glm::vec3 const &step) const;
+
+	//used to read back results of walking:
+	glm::vec3 world_point(WalkPoint const &wp) const {
+		return wp.weights.x * vertices[wp.triangle.x]
+		     + wp.weights.y * vertices[wp.triangle.y]
+		     + wp.weights.z * vertices[wp.triangle.z];
+	}
+
+	glm::vec3 world_normal(WalkPoint const &wp) const {
+		//TODO: could interpolate vertex_normals instead of computing the triangle normal:
+		return glm::normalize(glm::cross(
+			vertices[wp.triangle.y] - vertices[wp.triangle.x],
+			vertices[wp.triangle.z] - vertices[wp.triangle.x]
+		));
+	}
+
+};
